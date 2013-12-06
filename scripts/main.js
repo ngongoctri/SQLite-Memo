674e1c951ab313988a857684334f02891d70247d
ngongoctri <ngongoctri@gmail.com>
2013-12-06 14:49:36 +0700

    - push project

---
+++ scripts/main.js
@@ -0,0 +1,90 @@
+//Based on http://www.html5rocks.com/en/tutorials/webdatabase/todo/
+
+document.addEventListener("deviceready", init, false);
+//Activate :active state on device
+document.addEventListener("touchstart", function() {}, false);
+
+var app = {};
+app.db = null;
+      
+app.openDb = function() {
+   if (device.uuid == "e0101010d38bde8e6740011221af335301010333" || device.uuid == "e0908060g38bde8e6740011221af335301010333") {
+        // For debugin in simulator fallback to native SQL Lite
+        console.log("Use built in SQL Lite");
+        app.db = window.openDatabase("Todo", "1.0", "Cordova Demo", 200000);
+    }
+    else {
+        app.db = window.sqlitePlugin.openDatabase("Todo");
+    }
+}
+      
+app.createTable = function() {
+	var db = app.db;
+	db.transaction(function(tx) {
+		tx.executeSql("CREATE TABLE IF NOT EXISTS todo(ID INTEGER PRIMARY KEY ASC, todo TEXT, added_on DATETIME)", []);
+	});
+}
+      
+app.addTodo = function(todoText) {
+	var db = app.db;
+	db.transaction(function(tx) {
+		var addedOn = new Date();
+		tx.executeSql("INSERT INTO todo(todo, added_on) VALUES (?,?)",
+					  [todoText, addedOn],
+					  app.onSuccess,
+					  app.onError);
+	});
+}
+      
+app.onError = function(tx, e) {
+	console.log("Error: " + e.message);
+} 
+      
+app.onSuccess = function(tx, r) {
+	app.refresh();
+}
+      
+app.deleteTodo = function(id) {
+	var db = app.db;
+	db.transaction(function(tx) {
+		tx.executeSql("DELETE FROM todo WHERE ID=?", [id],
+					  app.onSuccess,
+					  app.onError);
+	});
+}
+
+app.refresh = function() {
+	var renderTodo = function (row) {
+		return "<li>" + "<div class='todo-check'></div>" + row.todo + "<a class='button delete' href='javascript:void(0);'  onclick='app.deleteTodo(" + row.ID + ");'><p class='todo-delete'></p></a>" + "<div class='clear'></div>" + "</li>";
+	}
+    
+	var render = function (tx, rs) {
+		var rowOutput = "";
+		var todoItems = document.getElementById("todoItems");
+		for (var i = 0; i < rs.rows.length; i++) {
+			rowOutput += renderTodo(rs.rows.item(i));
+		}
+      
+		todoItems.innerHTML = rowOutput;
+	}
+    
+	var db = app.db;
+	db.transaction(function(tx) {
+		tx.executeSql("SELECT * FROM todo", [], 
+					  render, 
+					  app.onError);
+	});
+}
+      
+function init() {
+    navigator.splashscreen.hide();
+	app.openDb();
+	app.createTable();
+	app.refresh();
+}
+      
+function addTodo() {
+	var todo = document.getElementById("todo");
+	app.addTodo(todo.value);
+	todo.value = "";
+}
